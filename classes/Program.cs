7e850907a87d795f61f3427ba558b4730460ffa3
Nadir Zamanov <Zamanov@itstep.edu.az>
2023-08-30 18:02:39 +0400

    Lesson 4

---
+++ classes/Program.cs
@@ -0,0 +1,52 @@
+﻿Human human = new Human();
+human.name = "Nadir";
+human.Foo();
+Human human2 = human;
+Console.WriteLine($"{human.name} {human2.name}");
+human2.name = "A-tilla";
+Console.WriteLine($"{human.name} {human2.name}");
+
+Console.WriteLine(Human.count);
+
+void Bar()
+{
+    Console.WriteLine("Salam");
+}
+public class Human
+{
+    // public, private, internal, protected internal
+    public string name; 
+    public string surname;
+    public int age;
+
+    public static int count;
+
+    public Human()
+        :this("Empty", "Empty", 0)
+    {}
+
+    public Human(string name, string surname, int age)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public void Foo()
+    {
+        Bambarbiya.Method(this);
+    }
+
+    //public void Bar(int a, double b) { }
+    //public void Bar(double a, int b) { }
+    //public void Bar(int a, int b) { }
+    //public void Bar(int a) { }
+}
+
+class Bambarbiya
+{
+    public static void Method(Human human){
+        Console.WriteLine(human.name);
+    }
+}
+
---
+++ structs/Program.cs
@@ -0,0 +1,51 @@
+﻿//Point point = new Point();
+//Console.WriteLine(point.X);
+//Console.WriteLine(point.Y);
+//Console.WriteLine(point.Bulbul);
+
+//Console.WriteLine();
+
+//var pointBlank = new Point(25, 23, true);
+//Console.WriteLine(pointBlank.X);
+//Console.WriteLine(pointBlank.Y);
+//Console.WriteLine(pointBlank.Bulbul);
+
+//Console.WriteLine();
+
+Point point1 = new(69, 89, false);
+Point point2 = point1;
+
+Console.WriteLine(point1.X);
+Console.WriteLine(point1.Y);
+Console.WriteLine(point1.Bulbul);
+Console.WriteLine();
+Console.WriteLine(point2.X);
+Console.WriteLine(point2.Y);
+Console.WriteLine(point2.Bulbul);
+Console.WriteLine();
+Console.WriteLine();
+
+point2.X = 156;
+
+Console.WriteLine(point1.X);
+Console.WriteLine(point1.Y);
+Console.WriteLine(point1.Bulbul);
+Console.WriteLine();
+Console.WriteLine(point2.X);
+Console.WriteLine(point2.Y);
+Console.WriteLine(point2.Bulbul);
+Console.WriteLine();
+Console.WriteLine();
+struct Point
+{
+    public int X;
+    public int Y;
+    public bool Bulbul;
+
+    public Point(int x, int y, bool bulbul)
+    {
+        X = x;
+        Y = y;
+        Bulbul = bulbul;
+    }
+}
